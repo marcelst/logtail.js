92720067209d13c8ea44ff00287ec848c2c17796
Marcel <marcelst@me.com>
2013-05-23 23:30:06 +0200

    use timers to serialize operations

--- logtail.js
+++ logtail.js
@@ -5,57 +5,56 @@ var scrollWithData = true;
 function tailf() {
      $.ajax({type: "GET",
             url: url,
-            headers : { "Range" : 'bytes=' + parseInt(lastByte) + "-" },
+            headers : { "Range" : 'bytes=' + (lastByte-1) + "-" },
             success : function(data, textStatus) {
-                lastByte += data.length
+                data = data.substr(1)
+                lastByte += data.length;
                 $("#thePlace").append(data);
-                
-                if (scrollWithData) {
-                //    $(window).scrollTop($('#page_bottom').offset().top);
-                }
             }
-           })
+           });
 }
       
-var timer;
+var timer, timer2;
 $(window).bind('scroll',function () {
-    clearTimeout(timer);
-    timer = setTimeout( refresh , 500);
-    console.log("scrolling")
-    var div = $('#page_bottom')
-    if($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
-        div.text("Following...")
-    } else {
-        div.text("Follow")
-    }
 });
 
-var $viewport = $('html, body');
-
-
-// Stop the animation if the user scrolls. Defaults on .stop() should be fin
-$viewport.bind("scroll mousedown DOMMouseScroll mousewheel keyup", function(e){
-    if ( e.which > 0 || e.type === "mousedown" || e.type === "mousewheel"){
-        //               $viewport.stop().unbind('scroll mousedown DOMMouseScroll mousewheel keyup'); 
-        jQuery.scrollTo.window().stop(true);
-        console.log("test")
-        // This identifies the scroll as a user action, stops the animation, then unbinds the event straight after (optional)
+$('body,html').bind('scroll mousedown wheel DOMMouseScroll mousewheel keyup', function(e){
+ if ( e.which > 0 || e.type == "mousedown" || e.type == "mousewheel"){
+  $("html,body").stop();
+       clearTimeout(timer);
+    clearTimeout(timer2);
+    timer = setTimeout(refresh , 100);
+    var div = $('#page_bottom');
+    if($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
+        div.text("Following...");
+        var div = $('#page_bottom');
+     //   $viewport.animate({ scrollTop: div.offset().top }, 5000);
+    } else {
+//        div.text("Follow");
     }
-});   
+ }
+})
+
+var $viewport = $('html, body');
+var $div = $('#page_bottom');
+
 
 var refresh = function () { 
     console.log('Stopped Scrolling');
     if($(window).scrollTop() + $(window).height() > $(document).height() - 200) {
-        scrollWithData = true
-        var div = $('#page_bottom')
-        console.log("near bottom!");
-        $('html,body').animate({ scrollTop: div.offset().top }, 5000);
+        scrollWithData = true;
+        $('html, body').animate({ scrollTop: $('#page_bottom').offset().top }, 1000, "swing", function() {
+           timer2 = setTimeout(follow, 100);
+        });
     } else {
-        scrollWithData = false
-        $('#page_bottom').text("Follow")
-        console.log("off!");
+        scrollWithData = false;
+        $('#page_bottom').text("Follow");
     }
 
 };
 
-
+var follow = function () {
+    $(window).scrollTop($('#page_bottom').offset().top);
+    $('html,body').dequeue("custom")
+    timer2 = setTimeout(follow, 100);
+}
